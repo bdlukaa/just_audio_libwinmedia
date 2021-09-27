b37813b914a5518078cd8edccaa4bc49598b5e4a
Bruno D'Luka <45696119+bdlukaa@users.noreply.github.com>
2021-09-27 20:05:06 -0300

    Updates

---
+++ example/lib/root.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Root extends StatelessWidget {
+  const Root({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
